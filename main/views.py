978f0a6b62a9043827c59a152cd98c7639f94a6b
m.isakov <make1ele@mail.ru>
2023-04-19 12:12:55 +0300

    add

--- main/views.py
+++ main/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from main.models import *
 # Create your views here.
 
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'base.html'
 
     def get(self, request, *args, **kwargs):
-        return render(self.request, self.template_name, {})
+        context = {}
+        categories = Category.objects.all()
+        context['categories'] = categories
+        context['active_category'] = None
+        return render(self.request, self.template_name, context)
+
+
+class CategoryView(TemplateView):
+    template_name = 'base.html'
+
+    def get(self, request, *args, **kwargs):
+        category = kwargs.get('category')
+        context = {}
+        categories = Category.objects.all()
+        context['categories'] = categories
+        context['active_category'] = category
+        return render(self.request, self.template_name, context)
